f9071e85d29ebd22c8b3ff20f3092eb46f20b635
Tejas Shende <tejasgshende@gmail.com>
2023-05-15 15:42:06 +0530

    Update Allotment.cpp

--- Allotment.cpp
+++ Allotment.cpp
@@ -26,10 +26,9 @@ int allotment() {
     /*I plan to first allot at least someone to each project, without considering the opportunity of mentorship*/
     int count=0;
     int i=0, j=0;
-    //while(i++<=n) allote(allot, i, shtml[i], spython[i], sdsa[i], sjava[i], ssql[i], m, phtml, ppython, pdsa, pjava, psql)
-    //i=0;
-    while (i++<=m) {
-        j=0;
+    
+    while (i++<=m) { //project-wise sequential allotment
+        j=0;    //checking for and filling html project role vacancy
         if(phtml[i]!=0) {
             while(j++<=n) {
                 if ((shtml[j] == phtml[i]) && (allot[j]==0)) {
@@ -78,7 +77,7 @@ int allotment() {
             }
         }
     
-        j=0;
+        j=0; //checking for and filling python project role vacancy
         if(ppython[i]!=0) {
             while(j++<=n) {
                 if ((spython[j] == ppython[i]) && (allot[j]==0)) {
@@ -127,7 +126,7 @@ int allotment() {
             }
         }
         
-        j=0;
+        j=0; //checking for and filling dsa project role vacancy
         if(pdsa[i]!=0) {
             while(j++<=n) {
                 if ((sdsa[j] == pdsa[i]) && (allot[j]==0)) {
@@ -174,8 +173,105 @@ int allotment() {
                     }
                 }
             }
+            
+            j=0; //checking for and filling java project role vacancy
+            if(pjava[i]!=0) {
+            while(j++<=n) {
+                if ((sjava[j] == pjava[i]) && (allot[j]==0)) {
+                allot[j] = i; break;
+                }
+            }
+            if(j==n+1) {
+                j=0;
+                if(pjava[i]+1<=5) {    
+                    while(j++<=n) {
+                        if ((sjava[j] == pjava[i] + 1) && (allot[j]==0)) {
+                            allot[j] = i; break;
+                        }
+                    }
+                }
+            }
+            if(j==n+1) {
+                j=0;
+                if(pjava[i]+2<=5) {    
+                    while(j++<=n) {
+                        if ((sjava[j] == pjava[i] + 2) && (allot[j]==0)) {
+                            allot[j] = i; break;
+                        }
+                    }
+                }
+            }
+            if(j==n+1) {
+                j=0;
+                if(pjava[i]+3<=5) {    
+                    while(j++<=n) {
+                        if ((sjava[j] == pjava[i] + 3) && (allot[j]==0)) {
+                            allot[j] = i; break;
+                        }
+                    }
+                }
+            }
+            if(j==n+4) {
+                j=0;
+                if(pjava[i]+4<=5) {    
+                    while(j++<=n) {
+                        if ((sjava[j] == pjava[i] + 4) && (allot[j]==0)) {
+                            allot[j] = i; break;
+                        }
+                    }
+                }
+            }
+               
+            j=0; //checking for and filling sql project role vacancy
+            if(psql[i]!=0) {
+            while(j++<=n) {
+                if ((ssql[j] == psql[i]) && (allot[j]==0)) {
+                allot[j] = i; break;
+                }
+            }
+            if(j==n+1) {
+                j=0;
+                if(psql[i]+1<=5) {    
+                    while(j++<=n) {
+                        if ((ssql[j] == psql[i] + 1) && (allot[j]==0)) {
+                            allot[j] = i; break;
+                        }
+                    }
+                }
+            }
+            if(j==n+1) {
+                j=0;
+                if(psql[i]+2<=5) {    
+                    while(j++<=n) {
+                        if ((ssql[j] == psql[i] + 2) && (allot[j]==0)) {
+                            allot[j] = i; break;
+                        }
+                    }
+                }
+            }
+            if(j==n+1) {
+                j=0;
+                if(psql[i]+3<=5) {    
+                    while(j++<=n) {
+                        if ((ssql[j] == psql[i] + 3) && (allot[j]==0)) {
+                            allot[j] = i; break;
+                        }
+                    }
+                }
+            }
+            if(j==n+1) {
+                j=0;
+                if(psql[i]+4<=5) {    
+                    while(j++<=n) {
+                        if ((ssql[j] == psql[i] + 4) && (allot[j]==0)) {
+                            allot[j] = i; break;
+                        }
+                    }
+                }
+            }
+         
         }
-    
+    i=0;
     while(i++<=n) {if(allot[i]!=0) count++;}
     return count;
 }
